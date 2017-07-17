6c532950f28fa3e4cd24585cb176af78617d9557
Deepak Kumar <Deepaknkumar@users.noreply.github.com>
2017-07-17 00:54:59 -0700

    Project Added!
    Project initialized: First Commit

---
+++ src/ObjectsAndClasses.java
@@ -0,0 +1,13 @@
+/**
+ * Created by deepakkumar on 2017-07-17.
+ */
+import java.util.*;
+
+public class ObjectsAndClasses {
+
+    public static void main(String[] args){
+        System.out.println(new Date());
+
+    }
+
+}
