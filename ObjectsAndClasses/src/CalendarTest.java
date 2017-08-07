057443ec8b618dd453ee48a64ecf8465cf75de37
Deepak Kumar <Deepaknkumar@users.noreply.github.com>
2017-08-06 23:15:31 -0700

    Calendar and Employee part
    Calendar and Employee part Added!

--- ObjectsAndClasses/src/CalendarTest.java
+++ ObjectsAndClasses/src/CalendarTest.java
@@ -8,11 +8,33 @@ public class CalendarTest {
 
     public static void main(String[] args){
 
-        LocalDate date = LocalDate.now();
+        LocalDate date = LocalDate.now();      // Today's date
         int month = date.getMonthValue();
         int today = date.getDayOfMonth();
 
-        date = date.minusDays(today-1);
-    }
+        date = date.minusDays(today - 1);       // Set to start of month
+        DayOfWeek weekday = date.getDayOfWeek();
+        int value = weekday.getValue();        // 1 = Monday, ... 7 = Sunday
 
+        System.out.println("Mon Tue Wed Thu Fri Sat Sun");
+        for (int i=1; i < value; i++){
+            System.out.print("   ");
+        }
+
+        while (date.getMonthValue() == month){
+            System.out.printf("%3d", date.getDayOfMonth());
+            if (date.getDayOfMonth() == today){
+                System.out.print("*");
+            }else{
+                System.out.print(" ");
+            }
+            date = date.plusDays(1);
+            if(date.getDayOfWeek().getValue() == 1){
+                System.out.println();
+            }
+        }
+        if (date.getDayOfWeek().getValue() != 1){
+            System.out.println();
+        }
+    }
 }
--- src/ObjectsAndClasses.java
+++ src/ObjectsAndClasses.java
@@ -2,11 +2,26 @@
  * Created by deepakkumar on 2017-07-17.
  */
 import java.util.*;
+import java.time.*;
 
 public class ObjectsAndClasses {
 
     public static void main(String[] args){
         System.out.println(new Date());
+
+        LocalDate date = LocalDate.now();
+        int month = date.getMonthValue();
+        int today = date.getDayOfMonth();
+
+        date = date.minusDays(today-1);
+
+        LocalDate newYearsEve = LocalDate.of(1994,12,31);
+        LocalDate aThousandDaysLater = newYearsEve.plusDays(1000);
+        int year = aThousandDaysLater.getYear();
+        month = aThousandDaysLater.getMonthValue();
+        int day = aThousandDaysLater.getDayOfMonth();
+
+        System.out.println(year +"\n"+ month+"\n" +day);
 
     }
 
