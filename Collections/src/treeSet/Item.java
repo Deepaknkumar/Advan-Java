40db5a499ae04cacefd5f4a30b22389d95665f29
Deepak Kumar <Deepaknkumar@users.noreply.github.com>
2017-10-15 14:35:29 -0700

    TreeSet
    TreeSet added!

---
+++ Collections/src/treeSet/Item.java
@@ -0,0 +1,17 @@
+package treeSet;
+
+import java.util.*;
+/**
+ * Created by deepakkumar on 2017-10-15.
+ * from book Core Java vol 1
+ */
+
+public class Item implements Comparable<Item>{
+
+    private String description;
+    private int partNumber;
+
+    public int compareTo(Item other){
+        return 0;
+    }
+}
---
+++ Collections/src/treeSet/TreeSetTest.java
@@ -0,0 +1,9 @@
+package treeSet;
+
+/**
+ * Created by deepakkumar on 2017-10-15.
+ * from book Core Java vol 1
+ */
+
+public class TreeSetTest {
+}
